0cede00dcc543505255296d31650b1cf71e8c54e
ryguyrg <ryan@ryugy.com>
2016-11-11 15:36:02 -0800

    typo

--- www/app.js
+++ www/app.js
@@ -54,11 +54,13 @@ $(document).ready(function() {
   var show_instances = function(instances) {
     alert(JSON.stringify(instances));
     var iList = $('ul.instances')
-    var li = $('<li/>');
+    var li = $('<li/>')
+      .appendTo(iList);
     var a = $('<a/>')
       .attr('href', 'http://localhost:7474/')
       .text(instances.usecase)
       .appendTo(li);
+    
   }
 
   var logout = function() {
