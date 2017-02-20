cc4a1dc6c8e6d23f6ab87f40b5ea94d45aef4d57
ryguyrg <ryan@ryguy.com>
2017-02-19 19:17:11 -0800

    added new pem file for neo4j-sync.auth0

--- s3-files/neo4j-twitter-config/neo4j-extension/extension_script.sh
+++ s3-files/neo4j-twitter-config/neo4j-extension/extension_script.sh
@@ -8,6 +8,6 @@ if [ -d "/usecase-datastores-ro/$USECASE.db" ]; then
  echo "dbms.active_database=$USECASE.db" >> $PWD/conf/neo4j.conf
 fi
 
-echo "browser.post_connect_cmd=play http://guides.neo4j.com/sandbox/$USECASE" >> $PWD/conf/neo4j.conf
+echo "browser.post_connect_cmd=play https://neo4jsandbox.com/guides/$USECASE/" >> $PWD/conf/neo4j.conf
 echo "dbms.connector.bolt.advertised_address=`curl -s \"https://ppriuj7e7i.execute-api.us-east-1.amazonaws.com/prod/SandboxGetInstanceByHashKey?sandboxHashKey=$SANDBOX_HASHKEY\"`" >> $PWD/conf/neo4j.conf
 python /backup-server.py &
